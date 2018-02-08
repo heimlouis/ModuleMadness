70f4b217c35d0cfb461b5c9cde08bf0531506592
Louis Heim <heimlouis@gmail.com>
2018-02-08 11:36:10 -0600

    removed notes from another assignment

--- server/public/client/currency.js
+++ server/public/client/currency.js
@@ -8,34 +8,3 @@ function currencyAmount(min, max){
     return money;
 }
 module.exports = currencyAmount;
-
-// $(document).ready(onReady);
-
-// function onReady(){
-//     console.log('Hello from jq');
-    
-//     $.ajax({
-//         type: 'GET', //type of request - 'get' also 'post'
-//         url: '/movies'
-//     }).done(function(response){
-//         //Do stuff when sucessful
-//         displayMovies(response);
-//         console.log('sucessful');
-//     }).fail(function(response){
-//         //If things go sideways on the server...
-//         console.log(response);
-//     });
-// }
-
-// function displayMovies(movies){ //movies is just a lable of reference, it could be anything
-//     $('#output-movies').empty();
-//     let $ul = $('<ul></ul>');
-//     for (let movie of movies){
-//         $ul.append(`<li>
-//         ${movie.title}, ${movie.director} - Rating: ${movie.rating}
-//         </li>`);
-//     }
-
-//     //output movies
-// $('#output-movies').append($ul);
-// }
